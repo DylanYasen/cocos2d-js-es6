ec19c641321e603e28c522fddbfc1539924babde
Yadikaer Yasheng <yadikae1@umbc.edu>
2017-02-26 21:16:12 -0500

    Init

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/game.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js',
+  },
+  module: {
+     loaders: [
+       {
+         test: /\.js$/,
+         loader: "babel-loader",
+       }
+     ]
+   },
+};
